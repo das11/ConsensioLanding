a2d892b83208d88c5d3f9873cbc5e1b7e621def1
das11 <kabird1196@gmail.com>
2015-11-15 22:47:19 +0530

    # countdown fix

--- move.js
+++ move.js
@@ -4,8 +4,19 @@ $('#bg-image').mousemove(function(e){
     $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
 });
 
+/*
 var clock = $('.clock').FlipClock(3600 * 24 * 51, {
 		clockFace: 'DailyCounter',
 		countdown: true,
 		showSeconds: true,
 	});
+*/
+
+var date = new Date(2016, 2, 5);
+    var now = new Date();
+    var diff = (date.getTime()/1000) - (now.getTime()/1000);
+
+    var clock = $('.clock').FlipClock(diff,{
+        clockFace: 'DailyCounter',
+        countdown: true
+    });  
